1adbe7d6717d2da29941a035a7ce7152ac3fac6e
Anna Wang <annawang378@gmail.com>
2025-07-11 10:52:07 -0400

    added birthday list

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+    // friends array
+    @State private var friends: [Friend] = [Friend(name: "Anna", birthday: .now), Friend(name: "Tracey", birthday: .now)]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                }
+            }
         }
-        .padding()
+        // closing the body
     }
 }
-
 #Preview {
     ContentView()
 }
